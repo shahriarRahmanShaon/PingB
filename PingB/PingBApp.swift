89287baea385d7daf7556df28930723d22be4d0b
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-19 22:54:19 +0600

    Initial Commit

---
+++ PingB/PingBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingBApp.swift
+//  PingB
+//
+//  Created by sergio shaon on 19/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PingBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
